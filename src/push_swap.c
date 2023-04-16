26248bb5fa52bcca117c4caa964fc81a801948f2
Cem Enes Karakus <ckarakus@k1m39s01.42istanbul.com.tr>
2023-04-16 03:44:44 +0300

    to do added

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,20 @@
 /*   By: ckarakus <ckarakus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 21:38:18 by ckarakus          #+#    #+#             */
-/*   Updated: 2023/04/16 03:40:24 by ckarakus         ###   ########.fr       */
+/*   Updated: 2023/04/16 03:44:32 by ckarakus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+/* Yapılanlar : Hata kontrolü neredeyse tamam, tekrar kontrol edilecek
+				Sort 3 yazıldı çalışıyor
+				Düzgün bir şekilde argüman alıp linked list oluşturuluyor
+
+Yapılacaklar : 5, 100 ve 500 rastgele sayılar için algoritma yazılacak
+				Argüman kabul ederken tek string halinde verilmesi sanırım
+				sorun çıkartıyor bunun fixlenmesi lazım.
+				Checker yazılacak */					
 
 int	main(int argc, char **argv)
 {
